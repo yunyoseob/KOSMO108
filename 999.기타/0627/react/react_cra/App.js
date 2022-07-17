b81a7f106058694854bb8bf13619b74ad1df8d53
Yun yoseob <81727895+yunyoseob@users.noreply.github.com>
2022-07-17 20:50:15 +0900

    Add files via upload

---
+++ 999.기타/0627/react/react_cra/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Start React WellCome !</h1>
+      <p>HTML 적용하기</p>
+    </div>
+  );
+}
+
+export default App;
