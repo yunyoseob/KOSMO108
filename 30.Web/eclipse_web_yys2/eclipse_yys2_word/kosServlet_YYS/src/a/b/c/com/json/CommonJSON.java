3a0cbb704552cced30c9e051d59b094e96591de2
Yun yoseob <81727895+yunyoseob@users.noreply.github.com>
2022-07-17 19:52:39 +0900

    Delete 30.Web/eclipse_web_yys2/eclipse_yys2_word directory

--- 30.Web/eclipse_web_yys2/eclipse_yys2_word/kosServlet_YYS/src/a/b/c/com/json/CommonJSON.java
+++
@@ -1,8 +0,0 @@
-package a.b.c.com.json;
-
-public abstract class CommonJSON {
-	
-	// file path 
-	public static final String JSON_FILE_PATH = "C:\\00.KOSMO108\\30.Web\\el_spring4_work\\kosmoSpring\\WebContent\\kos_json";
-
-}
